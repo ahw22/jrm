c4b2a33341f24b8d2f46eba5cbbf761d909687e8
ahwz <andreas.zincke@gmx.at>
2025-07-16 11:15:05 +0200

    initial commit

---
+++ src/main/java/at/ahwz/JRM/JrmApplication.java
@@ -0,0 +1,13 @@
+package at.ahwz.JRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ahwz/JRM/JrmApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ahwz.JRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
