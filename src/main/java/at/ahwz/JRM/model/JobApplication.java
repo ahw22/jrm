b1e3a40a5d9b22bb95c3c41ff82d3b019ea51037
ahwz <andreas.zincke@gmx.at>
2025-07-16 11:49:38 +0200

    update annotations on JobApplication

--- src/main/java/at/ahwz/JRM/model/JobApplication.java
+++ src/main/java/at/ahwz/JRM/model/JobApplication.java
@@ -4,14 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
 @Builder
-@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobApplication {
 
     @Id
