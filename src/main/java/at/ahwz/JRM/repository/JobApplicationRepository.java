f98e0ac00dfbcb78563cf1a4f03645464fed6998
ahwz <andreas.zincke@gmx.at>
2025-07-16 11:43:45 +0200

    add service annotation

--- src/main/java/at/ahwz/JRM/repository/JobApplicationRepository.java
+++ src/main/java/at/ahwz/JRM/repository/JobApplicationRepository.java
@@ -2,6 +2,8 @@ package at.ahwz.JRM.repository;
 
 import at.ahwz.JRM.model.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
 }
